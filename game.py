3e911509f9aac73a12a2a3a215a5070de005d1b2
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-11-14 19:57:56 +0330

    fixed some bugs

--- game.py
+++ game.py
@@ -1,22 +1,25 @@
 from enum import Enum
-from copy import copy as cp
-
 
 WIDTH = 4
-values = [2 ** i for i in range(1, 12)] # [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+values = [2 ** i for i in range(1, 12)]  # [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 
+
+# ==============================
+#           Exceptions
+# ==============================
+
+class UniqueBlockIdError(Exception):
+    """Block id should be unique"""
 
 
 # -----------------------------
 #           Functions
 # -----------------------------
 
-def first(l: list, condition):
-    for i in l:
+def first(items: list, condition):
+    for i in items:
         if condition(i):
             return i
-
-
 
 
 # ==============================
@@ -26,14 +29,12 @@ def first(l: list, condition):
 class Position:
 
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-    
+        self.x: int = x
+        self.y: int = y
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Position):
             return self.x == other.x and self.y == other.y
-
 
 
 # ==============================
@@ -43,26 +44,14 @@ class Position:
 class Block:
 
     def __init__(self,
-                 id: int,
+                 block_id: int,
                  value: int):
-        self.id = id
+        self.block_id = block_id
         self.value = value
-    
-
-    def increment(self):
-        new = cp(self)
-        new.value = self.value + 1
-        return new
-
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Block):
-            return self.id == other.id
-
-
-
-
-
+            return self.block_id == other.block_id
 
 
 # ==============================
@@ -83,42 +72,40 @@ class Set:
 # ==============================
 
 class Board:
-    
 
     def __init__(self):
-        self.board = [[None] * 4] * 4
+        self.board = [[] * WIDTH] * WIDTH
 
-
-    def get_sets(self) -> list: # returns a touple of (block, position)
+    def get_sets(self) -> list:  # returns a tuple of (block, position)
         pass
 
-
     def first(self, condition) -> Set:
-        for set in self.get_sets:
-            if condition(set):
-                return set
+        for s in self.get_sets():
+            if condition(s):
+                return s
 
-
-    def findIf(self, condition) -> Position:
+    def find_if(self, condition) -> Position:
         for x in range(0, WIDTH):
             for y in range(0, WIDTH):
                 if condition(self.board[x][y]):
                     return Position(x, y)
 
-
-    def increment_block(self, block) -> Set:
-        pos = self.findIf(lambda b: b == block)
-        self.board[pos.x][pos.y] = block.increment()
-    
+    def increment_block(self, block):
+        pos = self.find_if(lambda b: b == block)
+        self.board[pos.x][pos.y].value += 1
 
     def update_position(self, block: Block, new_position: Position):
-        pos = self.findIf(lambda b: b == block)
+        pos = self.find_if(lambda b: b == block)
         self.board[pos.x][pos.y] = None
         self.board[new_position.x][new_position.y] = block
 
+    def add_block(self, s: Set):
+        if self.first(lambda x: x.block_id == s.block.block_id):
+            raise UniqueBlockIdError
+        self.board[s.position.x][s.position.y] = s.block
 
     def __str__(self) -> str:
-        # TODO
+        # TODO display as string for debugging
         pass
 
 
@@ -133,22 +120,18 @@ class Direction(Enum):
     DOWN = "D"
 
 
-
 # ==============================
 #         Game View Model
 # ==============================
 
 # viewmodel layer
 class GameViewModel:
-    
+
     def __init__(self):
         self.board = Board()
 
-    
     def move(self, direction: Direction):
         pass
-
-
 
 
 # ==============================
@@ -161,14 +144,17 @@ class GameView:
     def __init__(self, viewmodel: GameViewModel):
         self.viewmodel = viewmodel
         self.board = Board()
-    
 
     def user_input(self, direction: Direction):
         self.viewmodel.move(direction)
-    
 
     def increment_value(self, block: Block):
         self.board.increment_block(block)
+        # TODO animate
+        pass
+
+    def new_block(self, s: Set):
+        self.board.add_block(s)
         # TODO animate
         pass
 
@@ -177,28 +163,23 @@ class GameView:
         new_sets = self.viewmodel.board.get_sets()
 
         for new_set in new_sets:
-            # old_set = self.board.first(lambda s: s.id == new_set.id)
-            old_set = first(old_set, lambda s: s.block == new_set.block)
+            old_set = first(old_sets, lambda s: s.block == new_set.block)
 
-            if not old_set: # block is new
-                # TODO create a new block
+            if not old_set:  # block is new
+                self.new_block(new_set)
                 continue
-            
+
             old_sets.remove(old_set)
 
-            if old_set.position == new_set.position: continue # block is in same place
+            if old_set.position == new_set.position:
+                continue  # block is in same place
 
-            else: # block is moved
+            else:  # block is moved
                 self.animate_to(old_set.block, new_set.position)
-            
+
             if old_set.block.value != new_set.block.value:
                 self.increment_value(old_set.block)
-
-
-
 
     def animate_to(self, block: Block, new_position: Position):
         # TODO update the position of block to new position with animation
         pass
-        
-
